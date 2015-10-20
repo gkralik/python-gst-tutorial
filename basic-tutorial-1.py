91b6f1ea54c5a3f0b5a916e0e5a68f591132d497
Gregor Kralik <g.kralik@gmail.com>
2015-10-20 16:39:29 +0200

    basic tutorial 1

---
+++ basic-tutorial-1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GObject, GLib
+
+pipeline = None
+bus = None
+message = None
+
+# initialize GStreamer
+Gst.init(None)
+
+# build the pipeline
+pipeline = Gst.parse_launch(
+    "playbin uri=http://docs.gstreamer.com/media/sintel_trailer-480p.webm"
+)
+
+# start playing
+pipeline.set_state(Gst.State.PLAYING)
+
+# wait until EOS or error
+bus = pipeline.get_bus()
+msg = bus.timed_pop_filtered(
+    Gst.CLOCK_TIME_NONE,
+    Gst.MessageType.ERROR | Gst.MessageType.EOS
+)
+
+# free resources
+pipeline.set_state(Gst.State.NULL)
